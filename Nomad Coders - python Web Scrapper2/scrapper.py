2b9747e09d6da5aa67d958d5c589a9d40d0c230d
lazybones1 <lazybones1@naver.com>
2020-04-11 17:24:50 +0900

    webscraper_ver2

--- Nomad Coders - python Web Scrapper2/scrapper.py
+++ Nomad Coders - python Web Scrapper2/scrapper.py
@@ -1,11 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
 
-
-def get_last_page():
-    result = requests.get(URL)
+def get_last_page(url):
+    result = requests.get(url)
     soup = BeautifulSoup(result.text, "html.parser")
     pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
     last_pages = pages[-2].get_text(strip=True)
@@ -14,8 +12,7 @@ def get_last_page():
 
 def extract_job(html):
     title = html.find("h2").find("a")["title"]
-    company, location = html.find("h3").find_all(
-        "span", recursive=False)
+    company, location = html.find("h3").find_all("span", recursive=False)
     company = company.get_text(strip=True)
     location = location.get_text(strip=True)
     job_id = html["data-jobid"]
@@ -23,15 +20,15 @@ def extract_job(html):
         'title': title,
         'company': company,
         'location': location,
-        'apply_link': f"https://stackoverflow.com/jobs/{job_id}"
+        'link': f"https://stackoverflow.com/jobs/{job_id}"
     }
 
 
-def extract_jobs(last_page):
+def extract_jobs(last_page, url):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping SO: page: {page}")
-        result = requests.get(f"{URL}&pg={page+1}")
+        result = requests.get(f"{url}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
         for result in results:
@@ -40,7 +37,8 @@ def extract_jobs(last_page):
     return jobs
 
 
-def get_jobs():
-    last_page = get_last_page()
-    jobs = extract_jobs(last_page)
+def get_jobs(word):
+    url = f"https://stackoverflow.com/jobs?q={word}&sort=i"
+    last_page = get_last_page(url)
+    jobs = extract_jobs(last_page, url)
     return jobs
